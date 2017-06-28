0249bb703ce29393fd1e33e50c60920a36449750
Justin O'Boyle <justin@justinoboyle.com>
2017-06-28 15:15:06 -0500

    change again

--- _inject.js
+++ _inject.js
@@ -1,11 +1,11 @@
-MYDISCORD_TEMP_CONFIG = JSON.parse(new Buffer(`${PAYLOAD}` || '', 'base64').toString('utf8'));
+global.MYDISCORD_TEMP_CONFIG = JSON.parse(new Buffer(`${PAYLOAD}` || '', 'base64').toString('utf8'));
 
 /** MYDISCORD-START */
 (function() {
     if(!window)
         window = global;
     window._MYDISCORD = {
-        _internalConfig: MYDISCORD_TEMP_CONFIG,
+        _internalConfig: global.MYDISCORD_TEMP_CONFIG,
         _beautifulDiscordCSSHook: () => console.log("No BeautifulDiscord CSS hook installed.")
     };
     window.applyAndWatchCSS = _MYDISCORD._beautifulDiscordCSSHook;
